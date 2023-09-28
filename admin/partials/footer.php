8c329ca12cdbdc59b16b9ce8953a145d90fc6ed3
Hanutomar <99379230+Hanutomar@users.noreply.github.com>
2023-09-28 09:39:48 -0400

    Add files via upload

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center"><?php echo date("Y"); ?> All rights reserved. Developed By Vijay Thapa</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
+</html>
